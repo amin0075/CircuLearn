ffc976f9b43fe0bfb89df31b931a6d4e09a19111
amin <aminkeshavarzi55@gmail.com>
2024-08-21 23:50:59 +0100

    fix: build fixed

--- src/@types/simulator.ts
+++
@@ -1,6 +0,0 @@
-// src/components/CustomNodes.tsx
-import LogicNode from "@src/components/global/Simulations/LogicCircuitSimulator/LogicNode";
-
-export const nodeTypes = {
-  logicNode: LogicNode,
-};
